042d614e27b2567d7c12b2fcd4b641dd6c4422da
blzzrd <blzzrd@10.20.81.68>
2017-02-13 18:19:38 -0600

    Moved Lift Auto docking code into it's own class

---
+++ src/LiftAutoDock.cpp
@@ -0,0 +1,88 @@
+#import liftAutoDock.h
+
+double liftAutoDock::getLADDrvMagCmd(){
+	return drvMagCmd;
+}
+
+double liftAutoDock::getLADDrvAngCmd(){
+	return drvAngCmd;
+}
+
+double liftAutoDock::getLADDrvRotCmd(){
+	return drvRotCmd;
+}
+
+liftAutoDock::liftAutoDock()
+{
+	targetLock = false;
+	leftTargetDistToImgCenter = 0;
+	rightTargetDistToImgCenter = 0;
+	liftAutoDockCmd = false;
+	drvRotCmd = 0;
+	drvAngCmd = 0;
+	drvMagCmd = 0;
+}
+
+liftAutoDock::calcLiftAutoDock(bool autoDockCommand, bool targetLockStatus, int leftTargetDisanceToImageCenter, int rightTargetDistanceToImageCenter){
+
+	liftAutoDockCmd = autoDockCommand;
+	targetLock = targetLockStatus;
+	leftTargetDistToImgCenter = leftTargetDisanceToImageCenter;
+	rightTargetDistToImgCenter = rightTargetDistanceToImageCenter;
+	
+//Abort the entire state machine if the auto dock command is removed
+//or if the target is not found/lost during docking
+if(liftAutoDockCmd == false) {
+	liftAutoDockState = DO_NOTHING;	
+} else if(liftAutoDockCmd == true && targetLock == false){
+	printf("AutoDock: No Target Found\n")
+	liftAutoDockState = DO_NOTHING;	
+}
+
+//************Automatic lift docking state machine**************
+switch (autoGearStateMachine) {
+	case DO_NOTHING:
+		if (liftAutoDockCmd == true && targetLock == true) {
+			liftAutoDockState = HORIZONTAL_LINEUP;
+		}
+		break;
+		
+	case HORIZONTAL_LINEUP:		
+		int pegDistToImgCenter = (leftTargetDistToImgCenter + rightTargetDistToImgCenter) / 2;
+		printf("AutoDock: Horizontal Lineup. CurrDist %i\n", pegDistToImgCenter);
+		
+		if(pegDistToImgCenter > horzLineUpTolerance){
+			drvAngCmd = 0; //Moving towards 0 degrees moves peg to the left in the image
+			drvMagCmd = horzLineUpDrvPwr;
+		} else if(pegDistToImgCenter < -horzLineUpTolerance){
+			drvAngCmd = 180; //Moving towards 180 degrees moves peg to the right in the image
+			drvMagCmd = horzLineUpDrvPwr;
+		} else {
+			drvMagCmd = 0;
+			liftAutoDockState = DRIVE_TO_SPRING;
+		}
+		
+		break;	
+		
+	case TURN_TO_SQUARE://not currently used
+		break;	
+		
+	case DRIVE_TO_SPRING:
+		int targetDistApart = (-leftTargetDistToImgCenter + rightTargetDistToImgCenter);
+		printf("AutoDock: Driving to Lift. CurrDist %i\n", targetDistApart);
+		
+		if(targetDistApart < distToLiftGoal){
+			drvAngCmd = 270;
+			drvMagCmd = driveToLiftPwr; 
+		} else {
+			drvMagCmd = 0;
+			liftAutoDockState = DONE;
+		}
+		break;	
+		
+	case DONE:
+		printf("AutoDock: Done\n");
+		break;
+	}	
+}
+		
---
+++ src/liftAutoDock.h
@@ -0,0 +1,41 @@
+
+class liftAutoDock
+{
+	private:
+		enum liftAutoDockStates {
+		//Driver has not yet engaged the autolock
+		DO_NOTHING,
+		//Make sure the target is in the camera's field of vision
+		TARGET_AQUIRED,
+		//Make the robot square against the desired target
+		TURN_TO_SQUARE,
+		//Line the robot up to drive straight into the spring
+		HORIZONTAL_LINEUP,
+		//Drive to the proper distance to unload the spring
+		DRIVE_TO_SPRING,
+		//Wait a few seconds, then let normal drive controls work again
+		DONE
+		};
+		
+		liftAutoDockStates liftAutoDockState;
+		
+		//inputs and outputs
+		bool targetLock;
+		int leftTargetDistToImgCenter, rightTargetDistToImgCenter;
+		bool liftAutoDockCmd;
+		double drvRotCmd, drvAngCmd, drvMagCmd;
+		
+		//Cals
+		double horzLineUpDrvPwr = .4;
+		double driveToLiftPwr = .4;
+		int horzLineUpTolerance = 10; //Target max distance from center of lift to center of image
+		int distToLiftGoal = 300; //When moving toward the lift peg for final docking, how far apart
+									//the targets have to be to indicate the robot has reached the goal
+	
+	public:
+		liftAutoDock();
+		void calcLiftAutoDock(bool autoDockCommand, bool targetLockStatus, int leftTargetDisanceToImageCenter, int rightTargetDistanceToImageCenter);
+		double getLADDrvMagCmd();
+		double getLADDrvAngCmd();
+		double getLADDrvRotCmd();
+};
